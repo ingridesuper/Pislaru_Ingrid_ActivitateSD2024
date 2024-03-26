afa943beb87191ace0285cb998eb0070ad9a0713
ingridesuper <127611982+ingridesuper@users.noreply.github.com>
2024-03-26 14:04:18 +0200

    Update Seminar_6.c

--- Seminar_6.c
+++ Seminar_6.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <malloc.h>
+#include<stdio.h>
+#include<malloc.h>
 #include <string.h>
 
 typedef struct Biblioteca Biblioteca;
@@ -8,112 +8,140 @@ typedef struct Nod Nod;
 typedef struct ListaDubla ListaDubla;
 
 struct Biblioteca {
-	char* nume;
-	int nrCarti;
-	int nrCititori;
+    char* nume;
+    int nrCarti;
+    int nrCititori;
 };
 
 struct Nod {
-	Biblioteca info:
-	Nod* next;
-	Nod* prev;
+    Biblioteca info;
+    Nod* prev;
+    Nod* next;
 };
 
 struct ListaDubla {
-	Nod* head;
-	Nod* tail;
+    Nod* head;
+    Nod* tail;
 };
 
-
 Biblioteca initializare(const char* nume, int nrCarti, int nrCititori) {
-	Biblioteca biblioteca;
-	biblioteca.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
-	strcpy(biblioteca.nume, nume);
-	biblioteca.nrCarti = nrCarti;
-	biblioteca.nrCititori = nrCititori;
-	return biblioteca;
+    Biblioteca b;
+    b.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+    strcpy(b.nume, nume);
+    b.nrCarti = nrCarti;
+    b.nrCititori = nrCititori;
+    return b;
 }
 
-void inserareInceput(ListaDubla* lista, Biblioteca b) {
-	Nod* nod = (Nod*)malloc(sizeof(Nod));
-	//data trecuta am avut deep copy - acum facem shallow
-	nod->info = b;
-	nod->next = lista->head;
-	nod->prev = NULL;
-	if (lista->head != NULL) {
-		lista->head->prev = nod;
-		lista->head = nod;
-	}
-	else {
-		lista->head = nod;
-		lista->tail = nod;
-	}
+void insereazaInceput(ListaDubla* lista, Biblioteca b) {
+    Nod* nod_adaugat = (Nod*)malloc(sizeof(Nod));
+    nod_adaugat->info = b;
+    nod_adaugat->prev = NULL;
+    nod_adaugat->next = lista->head;
+
+    if (lista->head != NULL) {
+        lista->head->prev = nod_adaugat;
+    }
+    else {
+        lista->tail = nod_adaugat;
+    }
+
+    lista->head = nod_adaugat;
 }
 
-void afisareListaDeLaInceput(ListaDubla lista) {
-	while (lista.head) {
-		printf("Biblioteca: %s, %d nr cititori, %d nr carti\n", lista.head->info.nume, lista.head->info.nrCarti, lista.head->info.nrCititori);
-		lista.head = lista.head->next;
-	}
-	printf("\n");
+void afiseazaListaInceput(ListaDubla lista) {
+    while (lista.head) {
+        printf("Nume: %s, nr. carti: %d, nr.cititori: %d\n", lista.head->info.nume, lista.head->info.nrCarti, lista.head->info.nrCititori);
+        lista.head = lista.head->next;
+    }
 }
 
-//poate fi primul, ultimul, singurul, sau intermediar
 void stergeNodDupaNume(ListaDubla* lista, const char* nume) {
-	Nod* aux = lista->head;
-	while (aux && strcmp(nume, aux->info.nume)!=0) {
-		aux = aux->next;
-	}
-	if (aux) {
-		if (aux == lista->head) {
-			if (aux == lista->tail) { //e unicul
-				lista->head = NULL;
-				lista->tail = NULL;
-			}
-			else { //primul
-				lista->head = lista->head->next;
-				lista->head->prev = NULL;
-			}
-		}
-		else if (aux==lista->tail){ //ultimul
-			aux->prev->next = NULL;
-			lista->tail = aux->prev;
-		}
-		else {
-			aux->next->prev = aux->prev;
-			aux->prev->next = aux->next;
-			//nu mai setam - oricum stergem aux
-		}
+    Nod* aux = lista->head;
+    
+    while (aux && strcmp(nume, aux->info.nume) != 0) {
+        aux = aux->next;
+    }
 
-		free(aux->info->nume);
-		free(aux);
-	}
+    if (aux) {
+        if (aux == lista->head) {
+            if (aux == lista->head) {
+                lista->head = NULL;
+                lista->tail = NULL;
+            }
+            else {
+                lista->head = lista->head->next;
+                lista->head->prev = NULL;
+            }
+        }
+        else if (aux == lista->tail) {
+            aux->prev->next = NULL;
+            lista->tail = aux->prev;
+        }
+        else {
+            aux->next->prev = aux->prev;
+            aux->prev->next = aux->next;
+        }
+
+
+        free(aux->info.nume);
+        free(aux);
+    }
+
 }
 
 void stergeLista(ListaDubla* lista) {
-	while (lista->head) {
-		free(lista->head->info.nume);
-		//ceva
-	}
-	//ceva
+    while (lista->head) {
+        free(lista->head->info.nume);
+        Nod* aux = lista->head;
+        lista->head = lista->head->next;
+        free(aux);
+    }
+
+    lista->head = NULL;
+    lista->tail = NULL;
 }
 
-int main() {
+int nrCartiTotal(ListaDubla lista) {
+    int rezultat = 0;
 
-	Biblioteca b1 = initializare("Nora Iuga", 1200, 200);
-	Biblioteca b2 = initializare("Nina Cassian", 500, 100);
-	Biblioteca b3 = initializare("Medeea Iancu", 2093, 1000);
-	ListaDubla listaDubla;
-	listaDubla.head = NULL;
-	listaDubla.tail = NULL;
-	inserareInceput(&listaDubla, b1);
-	inserareInceput(&listaDubla, b2);
-	inserareInceput(&listaDubla, b3);
-	afisareListaDeLaInceput(listaDubla);
-	stergeNodDupaNume(&listaDubla, "Nina Cassian");
-	afisareListaDeLaInceput(listaDubla);
-	stergeNodDupaNume(&listaDubla, "Nora Iuga");
-	afisareListaDeLaInceput(listaDubla);
-	stergeNodDupaNume(&listaDubla, "Medeea Iancu");
-	afisareListaDeLaInceput(listaDubla);
+    while (lista.tail) {
+        rezultat += lista.tail->info.nrCarti;
+        lista.tail = lista.tail->prev;
+    }
+
+    return rezultat;
+}
+
+void main() {
+    Biblioteca b1 = initializare("Mihai Eminescu", 150, 30);
+    Biblioteca b2 = initializare("Ioan Slavici", 200, 30);
+    Biblioteca b3 = initializare("Tudor Arghezi", 100, 15);
+    
+    ListaDubla listaDubla;
+    listaDubla.head = NULL;
+    listaDubla.tail = NULL;
+
+
+    insereazaInceput(&listaDubla, b1);
+    insereazaInceput(&listaDubla, b2);
+    insereazaInceput(&listaDubla, b3);
+
+    afiseazaListaInceput(listaDubla);
+
+    //stergeNodDupaNume(&listaDubla, "Ioan Slavici");
+    //printf("\n\nStergere mijloc:\n");
+    //afiseazaListaInceput(listaDubla);
+
+    //stergeNodDupaNume(&listaDubla, "Mihai Eminescu");
+    //printf("\n\nStergere inceput:\n");
+    //afiseazaListaInceput(listaDubla);
+
+    //stergeNodDupaNume(&listaDubla, "Tudor Arghezi");
+    //printf("\n\nStergere final: \n");
+    //afiseazaListaInceput(listaDubla);
+
+    printf("%d carti in total", nrCartiTotal(listaDubla));
+
+    stergeLista(&listaDubla);
 }
